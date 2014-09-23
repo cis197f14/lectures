326c95e4b6d1ecbd547a4ca594d65804bc9f4d5b
Geoffrey Vedernikoff <yefim323@gmail.com>
2014-09-23 17:10:02 -0400

    Added lec3

---
+++ lec3/lec3.js
@@ -0,0 +1,68 @@
+// browsers
+
+// HTML
+/*
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel='styleshet' href='styles/style.css'>
+  </head>
+  <body>
+    <div>
+      <h1>Content</h1>
+      <p>Paragraphs #1</p>
+    </div>
+  </body>
+</html>
+
+              html
+            /    \
+          head  body
+                /
+               div
+               /  \
+              h1    p 
+              |      \
+          Content    Paragraphs #1
+
+*/
+
+// Loading JS in the browser
+/*
+<html>
+  <head>
+  </head>
+  <body>
+    <!-- method #1 -->
+    <script>
+      console.log('test');
+    </script>
+
+    <!-- method #2 (preferred) -->
+    </script src='scripts/hello.js'></script>
+    </body>
+</html>
+*/
+
+/*
+ *
+ * FOR THE REST OF THE LECTURE
+ * SEE THE test.html FILE
+ *
+ */
+
+// DOM (document object model)
+
+// DOM manipulation
+
+// attributes with getAttribute and setAttribute
+
+// styles
+
+// changing content with innerHTML
+
+// creating nodes with createElement
+
+// jQuery
+
+// selecting, manipulating
